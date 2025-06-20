51cda10649194bef79dfdf52c652861c68ac92e2
ahmedgaafer1 <midogaafer67@gmail.com>
2025-06-20 14:26:12 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 19 }}>
+        Todo App{" "}
+      </Text>
+      <TextInput style={styles.input} placeholder="Enter Title" />
+      <TextInput style={styles.input} placeholder="Enter Description" />
+      <TouchableOpacity style={styles.submitBtn} activeOpacity={0.7}>
+        <Text style={{ ...styles.text, fontWeight: "bold" }}>Submit </Text>
+      </TouchableOpacity>
+      <View style={styles.dividerLine} />
+      <View style={styles.filterContainer}>
+        <TouchableOpacity style={styles.activeFilterBtn} activeOpacity={0.7}>
+          <Text style={styles.activeFilterText}>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn} activeOpacity={0.7}>
+          <Text style={styles.filterText}>Active</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.filterBtn} activeOpacity={0.7}>
+          <Text style={styles.filterText}>Done</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +37,78 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    paddingBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "90%",
+    marginVertical: 10,
+    height: 50,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#fff",
+  },
+  submitBtn: {
+    width: "50%",
+    backgroundColor: "#2e86de",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 10,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+  dividerLine: {
+    height: 1,
+    width: "90%",
+    backgroundColor: "#ccc",
+    marginVertical: 15,
+  },
+  filterContainer: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+  },
+  filterBtn: {
+    width: "30%",
+    backgroundColor: "#ffffff",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#2e86de",
+  },
+  filterText: {
+    color: "#2e86de",
+    fontSize: 15,
+  },
+  activeFilterBtn: {
+    width: "30%",
+    backgroundColor: "#2e86de",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#2e86de",
+  },
+  activeFilterText: {
+    color: "white",
+    fontSize: 15,
+  },
+  todosContainer: {
+    marginTop: 10,
+  },
+  doneTodo: {
+    textDecorationLine: "line-through",
+    color: "#888",
   },
 });
